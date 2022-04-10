5ca881ae2a7194097dd7e75cbdd4451e40fd7074
刘文秀 <liuwx@tydic.com>
2022-04-10 23:01:13 +0800

    xml p-namespace和c-namespace

--- spring-04-di/src/test/java/tech/xiu/MyTest.java
+++ spring-04-di/src/test/java/tech/xiu/MyTest.java
@@ -1,8 +1,10 @@
 package tech.xiu;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import tech.xiu.bean.Student;
+import tech.xiu.bean.User;
 
 public class MyTest {
 
@@ -10,5 +12,14 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = context.getBean("student", Student.class);
         System.out.println(student);
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbean.xml");
+        User user = context.getBean("user", User.class);
+        User user2 = context.getBean("user2", User.class);
+        System.out.println(user);
+        System.out.println(user2);
     }
 }
