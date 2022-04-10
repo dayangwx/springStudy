505acfa5ac95aea8ca1ff60257131ca4d56326f2
刘文秀 <liuwx@tydic.com>
2022-04-10 22:25:04 +0800

    xml 复杂类型依赖注入

---
+++ spring-04-di/src/main/java/tech/xiu/bean/Address.java
@@ -0,0 +1,9 @@
+package tech.xiu.bean;
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+    private String address;
+}
---
+++ spring-04-di/src/test/java/tech/xiu/MyTest.java
@@ -0,0 +1,14 @@
+package tech.xiu;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tech.xiu.bean.Student;
+
+public class MyTest {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = context.getBean("student", Student.class);
+        System.out.println(student);
+    }
+}
