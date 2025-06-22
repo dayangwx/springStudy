4910b2e7085b9dd1038c096ea30a5c2a2592dbf2
liuwx <971465407@qq.com>
2025-06-22 16:05:14 +0800

    Remove .idea directory from tracking

---
+++ spring-03-ioc2/src/main/java/tech/xiu/config/SpringConfigForAnno.java
@@ -0,0 +1,14 @@
+package tech.xiu.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextBean {
+
+//    @Bean
+//    public ApplicationContext applicationContext() {
+//    }
+}
